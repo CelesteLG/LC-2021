847ea6a5e94aba82d39ee2cf451c27dc8dc3e5ea
CelesteLG <celeste.lorenzo@ciencias.unam.mx>
2020-10-27 14:12:39 -0600

    Actualizando

---
+++ Practica01/Variables.hs
@@ -0,0 +1,11 @@
+module Variables
+where
+
+data Var = Variable String deriving (Show)
+
+--EJEMPLOS 
+-- *Variables> let var = Variable "q"
+-- *Variables> var    --(La llamas)
+-- Variable "q"       ---(Lo que imprime)
+
+
